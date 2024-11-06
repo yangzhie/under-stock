3588b3cc14ffe3b9e2b47937fce2ddb95a038e10
ArnavMalasi <arnavmalasi2212@gmail.com>
2024-11-06 11:11:32 +1100

    Backend[Feat]: delete stock API.

--- server/index.js
+++ server/index.js
@@ -197,6 +197,30 @@ app.post("/:username/watchlist/:watchlistId/stock", authToken, async (req, res) 
     }
 });
 
+// Delete Stock
+app.delete('/:username/watchlist/:watchlistId/stock/:symbol', async (req, res) => {
+  const { username, watchlistId, symbol } = req.params;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.status(404).json({ error: true, message: "User not found." });
+    }
+
+
+    const watchlist = user.watchlists.find((watchlist) => watchlist._id.toString() === watchlistId);
+    if (!watchlist) {
+        return res.status(404).json({ error: true, message: "Watchlist not found." });
+    }
+
+    watchlist.stocks = watchlist.stocks.filter(stock => stock.symbol !== symbol);
+    await user.save();
+    res.status(200).json({ error: false, message: "Stock removed.", watchlist });
+  } catch (error) {
+    res.status(500).json({ message: "Error removing stock.", error });
+  }
+});
+
 // Listen on port
 app.listen(port, () => {
     console.log(`Server is now listening on port: ${port}`);
